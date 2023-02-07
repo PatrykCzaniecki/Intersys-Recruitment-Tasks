2e7c6e9d1eaa46d3954494d3ba3f953c86505482
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2023-02-07 12:20:30 +0100

    Task 2 complete

--- IntersysTaskPalindrome/Program.cs
+++ IntersysTaskPalindrome/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace IntersysTaskPalindrome;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Provide string to check: ");
+        var input = Console.ReadLine();
+        var cleanInput = string.Concat(Array.FindAll(input.ToCharArray(), char.IsLetter)).ToLower();
+        var result = CheckPalindrome(cleanInput);
+        Console.WriteLine(result ? "YES" : "NO");
+    }
+
+    public static bool CheckPalindrome(string cleanInput)
+    {
+        var reverseCleanInput = Reverse(cleanInput);
+        return cleanInput == reverseCleanInput;
+    }
+
+    public static string Reverse(string input)
+    {
+        var inputArray = input.ToCharArray();
+        Array.Reverse(inputArray);
+        return new string(inputArray);
+    }
+}
