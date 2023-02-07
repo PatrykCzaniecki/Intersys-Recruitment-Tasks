5bed81c6808fe17485b375b99c4d9e94d419241f
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2023-02-07 15:23:55 +0100

    Task 5 complete

---
+++ IntersysTaskFindingPrimes/IntersysTaskFindingPrimes/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace IntersysTaskFindingPrimes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var testCases = int.Parse(Console.ReadLine()!);
+
+        while (testCases --> 0)
+        {
+            var inputs = Console.ReadLine()!.Split();
+            var m = int.Parse(inputs[0]);
+            var n = int.Parse(inputs[1]);
+            var count = 0;
+
+            for (var i = m; i <= n; i++)
+            {
+                if (IsPrime(i))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+
+    private static bool IsPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+
+        for (var i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
