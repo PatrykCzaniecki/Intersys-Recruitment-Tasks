06f6976686621bf1a06c08e5aa08cb07109ddc1d
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2023-02-07 11:38:48 +0100

    Task 1 complete

--- IntersysTask1/Program.cs
+++ IntersysTask1/Program.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿namespace IntersysTask1;
 
-namespace IntersysTask1;
-
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello");
+        Console.Write("Provide size of your input: ");
+        var size = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Provide chars to reverse: ");
+        var numbers = Console.ReadLine();
+        var trimmedNumbers = string.Concat((numbers ?? throw new InvalidOperationException()).Where(space => !char.IsWhiteSpace(space)));
+        if(trimmedNumbers.Length == size) 
+        {
+            var reversedNumbers = Reverse(trimmedNumbers);
+            var reversedAndJoinedNumbers = Join(reversedNumbers);
+            Console.Write("Reversed chars are: " + reversedAndJoinedNumbers);
+        }
+        else
+        {
+            Console.WriteLine("Size of input is not equal to length of chars to reverse! Please try again.");
+        }
+    }
+
+    public static string Reverse(string numbers)
+    {
+        var numbersArray = numbers.ToCharArray();
+        Array.Reverse(numbersArray);
+        return new string(numbersArray);
+    }
+
+    public static string Join(string textToJoin)
+    {
+        var chars = textToJoin.ToCharArray();
+        return string.Join(" ", chars);
     }
 }
