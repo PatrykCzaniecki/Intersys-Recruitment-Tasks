2f0f40721aea8843dfe67b3a19bdc762d021e3e1
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2023-02-08 13:33:50 +0100

    Task 8 complete

---
+++ IntersysTaskCommonDigit/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IntersysTaskCommonDigit;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Provide number of numbers: ");
+        var inputNumber = int.Parse(Console.ReadLine()!);
+        Console.WriteLine($"Provide {inputNumber} numbers to check: ");
+        
+        var numbersArray = Console.ReadLine()!.ToCharArray();
+        var newNumbersArray = numbersArray.Where(number => number != ' ').ToArray();
+        CommonDigit(newNumbersArray);
+    }
+
+    private static void CommonDigit(IEnumerable<char> numbersArray)
+    {
+        var counts = new Dictionary<char, int>();
+        foreach (var number in numbersArray)
+        {
+            if (!counts.ContainsKey(number))
+            {
+                counts[number] = 0;
+            }
+            counts[number]++;
+        }
+
+        var maxKey = counts.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+        Console.WriteLine(maxKey);
+    }
+}
