22b560f83f4a949a31b34921ff1b3e0301f69ca6
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2023-02-09 14:44:36 +0100

    Task 9 complete

---
+++ IntersysTaskDigitSum/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace IntersysTaskDigitSum;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Provide number of numbers to check: ");
+        var inputNum = int.Parse(Console.ReadLine()!);
+        Console.WriteLine($"Provide {inputNum} numbers to check: ");
+        var numbers = Console.ReadLine()!.Split(' ');
+        var numbersArray = Array.ConvertAll(numbers, int.Parse);
+        
+        var maxDigitSum = 0;
+        var maxDigitSumIndex = 0;
+        for (var i = 0; i < inputNum; i++)
+        {
+            var digitSum = DigitSum(numbersArray[i]);
+            if (digitSum > maxDigitSum)
+            {
+                maxDigitSum = digitSum;
+                maxDigitSumIndex = i;
+            }
+        }
+        Console.WriteLine(maxDigitSumIndex);
+    }
+
+    private static int DigitSum(int number)
+    {
+        var digitSum = 0;
+        while (number > 0)
+        {
+            digitSum += number % 10;
+            number /= 10;
+        }
+        return digitSum;
+    }
+}
